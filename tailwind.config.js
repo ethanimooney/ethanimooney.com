1186c8f937e6a27675587af8964011d92317b728
ethanimooney <ethanimooney@gmail.com>
2021-12-11 21:29:13 -0600

    Initial theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-gray': '#313638',
+        'off-white': '#e0dfd5',
+        'gold': '#e4b363',
+        'red-orange': '#ee6461',
+      },
+      fontFamily: {
+        sans: ['Classica-Pro', 'sans-serif'],
+        serif: ['Bely-Display', 'serif'],
+      }
+    },
   },
   plugins: [],
 }
